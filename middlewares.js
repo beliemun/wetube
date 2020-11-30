8fe979448cf48a4205226322648c27fa860b1ba7
KIMJINHWAN <beliemun@paran.com>
2020-11-30 21:49:17 +0900

    Layout Update

--- middlewares.js
+++ middlewares.js
@@ -5,18 +5,19 @@ const multerVideo = multer({ dest: "uploads/videos/" }); // 업로드 될 서버
 export const uploadVideo = multerVideo.single("videoFile");
 
 export const localsMiddleware = (req, res, next) => {
-  // locals를 이용해 전역 변수를 추가한다.
-  res.locals.siteName = "WeTube";
-  res.locals.routes = routes;
-  res.locals.user = {
-    isAuthenticated: true,
-    // isAuthenticated: false,
-    id: 1,
-  };
-  // 정책상 영상을 로드하지 못함
-  res.setHeader(
-    "Content-Security-Policy",
-    "script-src 'self' https://archive.org"
-  );
-  next(); // 다음 코드로 넘어가기 위해서 반드시 next()사용
+    // locals를 이용해 전역 변수를 추가한다.
+    res.locals.siteName = "WeTube";
+    res.locals.routes = routes;
+    res.locals.user = {
+        isAuthenticated: true,
+        // isAuthenticated: false,
+        id: 1,
+        subscribeStatus: false,
+    };
+    // 정책상 영상을 로드하지 못함
+    res.setHeader(
+        "Content-Security-Policy",
+        "script-src 'self' https://archive.org"
+    );
+    next(); // 다음 코드로 넘어가기 위해서 반드시 next()사용
 };
