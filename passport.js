3d1a7b896ac3b13a043e360a7829d8a60e8b66cd
Jinhwan Kim <beliemun@paran.com>
2020-12-11 10:42:44 +0900

    update after git rm -r --cached .

--- passport.js
+++ passport.js
@@ -3,7 +3,10 @@ import GithubStrategy from "passport-github";
 import KakaoStrategy from "passport-kakao";
 
 import User from "./models/User";
-import { githubLoginCallback, kakaoLoginCallback } from "./controllers/userController";
+import {
+  githubLoginCallback,
+  kakaoLoginCallback,
+} from "./controllers/userController";
 import routes from "./routes";
 
 // strategy : 로그인을 하는 방법
@@ -13,23 +16,25 @@ import routes from "./routes";
 passport.use(User.createStrategy());
 
 passport.use(
-    new GithubStrategy({
-            clientID: process.env.GITHUB_CLIENT_ID,
-            clientSecret: process.env.GITHUB_CLIENT_SECRET,
-            callbackURL: `http://localhost:4000${routes.githubCallback}`
-        },
-        githubLoginCallback
-    )
+  new GithubStrategy(
+    {
+      clientID: process.env.GITHUB_CLIENT_ID,
+      clientSecret: process.env.GITHUB_CLIENT_SECRET,
+      callbackURL: `http://localhost:4000${routes.githubCallback}`,
+    },
+    githubLoginCallback
+  )
 );
 passport.use(
-    new KakaoStrategy({
-            clientID: process.env.KAKAO_CLIENT_ID,
-            clientSecret: process.env.KAKAO_CLIENT_SECRET, // clientSecret을 사용하지 않는다면 넘기지 말거나 빈 스트링을 넘길 것
-            callbackURL: `http://localhost:4000${routes.kakaoCallback}`
-        },
-        kakaoLoginCallback
-    )
-)
+  new KakaoStrategy(
+    {
+      clientID: process.env.KAKAO_CLIENT_ID,
+      clientSecret: process.env.KAKAO_CLIENT_SECRET, // clientSecret을 사용하지 않는다면 넘기지 말거나 빈 스트링을 넘길 것
+      callbackURL: `http://localhost:4000${routes.kakaoCallback}`,
+    },
+    kakaoLoginCallback
+  )
+);
 
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
