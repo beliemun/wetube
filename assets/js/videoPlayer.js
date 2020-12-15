6177b66536f0b25e013f0dead664686725926664
Jinhwan Kim <beliemun@paran.com>
2020-12-15 16:10:28 +0900

    #8.6 Volume Bar part One

--- assets/js/videoPlayer.js
+++ assets/js/videoPlayer.js
@@ -1,22 +1,104 @@
-import { initialize } from "passport";
-
 const videoContainer = document.getElementById("jsVideoPlayer");
 const videoPlayer = document.querySelector("#jsVideoPlayer video");
 const playButton = document.getElementById("jsPlayButton");
+const volumeButton = document.getElementById("jsVolumeButton");
+const fullScreenButton = document.getElementById("jsFullScreen");
+const currentTime = document.getElementById("jsCurrentTime");
+const totalTime = document.getElementById("jsTotalTime");
+const volumeRange = document.getElementById("jsVolumeRange");
+
+const formatDate = (duration) => {
+  const seconds = parseInt(duration, 10);
+  let hours = Math.floor(seconds / 3600);
+  let minutes = Math.floor((seconds - hours * 3600) / 60);
+  let totalSeconds = parseInt(seconds - hours * 3600 - minutes * 60, 10);
+
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+  if (seconds < 10) {
+    totalSeconds = `0${totalSeconds}`;
+  }
+  return `${hours}:${minutes}:${totalSeconds}`;
+};
+
+const getCurrentTime = () => {
+  currentTime.innerHTML = formatDate(Math.floor(videoPlayer.currentTime));
+};
+
+const setTotalTime = () => {
+  const totalTimeString = formatDate(videoPlayer.duration);
+  totalTime.innerHTML = totalTimeString;
+  setInterval(getCurrentTime, 1000);
+};
 
 const handlePlayClick = () => {
-    if (videoPlayer.paused) {
-        videoPlayer.play();
-    } else {
-        videoPlayer.pause();
-    }
-}
+  if (videoPlayer.paused) {
+    videoPlayer.play();
+    playButton.innerHTML = '<i class="fas fa-play"></i>';
+  } else {
+    videoPlayer.pause();
+    playButton.innerHTML = '<i class="fas fa-pause"></i>';
+  }
+};
+
+const handleVolumeClick = () => {
+  if (videoPlayer.muted) {
+    videoPlayer.muted = false;
+    volumeButton.innerHTML = '<i class="fas fa-volume-up"></i>';
+    volumeRange.value = videoPlayer.volume;
+  } else {
+    volumeRange.value = 0;
+    videoPlayer.muted = true;
+    volumeButton.innerHTML = '<i class="fas fa-volume-mute"></i>';
+  }
+};
+
+const exitFullScreen = () => {
+  fullScreenButton.innerHTML = '<i class="fas fa-expand"></i>';
+  fullScreenButton.addEventListener("click", goFullScreen);
+  document.exitFullscreen().catch((err) => Promise.resolve(err));
+};
+
+const goFullScreen = () => {
+  videoContainer.requestFullscreen();
+  fullScreenButton.innerHTML = '<i class="fas fa-compress"></i>';
+  fullScreenButton.removeEventListener("click", goFullScreen);
+  fullScreenButton.addEventListener("click", exitFullScreen);
+};
+
+const handleEnded = () => {
+  videoPlayer.currentTime = 0;
+  playButton.innerHTML = '<i class="fas fa-play"></i>';
+};
+
+const handleDrag = (event) => {
+  const {
+    target: { value },
+  } = event;
+  videoPlayer.volume = value;
+  if (value > 0.6) {
+    volumeButton.innerHTML = '<i class="fas fa-volume-up"></i>';
+  } else if (value > 0.2) {
+    volumeButton.innerHTML = '<i class="fas fa-volume-down"></i>';
+  } else {
+    volumeButton.innerHTML = '<i class="fas fa-volume-off"></i>';
+  }
+};
 
 const init = () => {
-    console.log("init");
-    playButton.addEventListener("click", handlePlayClick);
-}
+  videoPlayer.volume = 0.5;
+  playButton.addEventListener("click", handlePlayClick);
+  volumeButton.addEventListener("click", handleVolumeClick);
+  fullScreenButton.addEventListener("click", goFullScreen);
+  videoPlayer.addEventListener("loadedmetadata", setTotalTime);
+  videoPlayer.addEventListener("ended", handleEnded);
+  volumeRange.addEventListener("input", handleDrag);
+};
 
 if (videoContainer) {
-    init();
+  init();
 }
--- webpack.config.js
+++ webpack.config.js
@@ -6,55 +6,57 @@ const MODE = process.env.WEBPACK_ENV;
 const ENTRY_FILE = path.resolve(__dirname, "assets", "js", "main.js");
 const OUTPUT_DIR = path.join(__dirname, "static");
 
-require("@babel/polyfill");
-
 const config = {
-    devtool: "cheap-module-source-map", // 에러나서 추가 됨
-    entry: ["@babel/polyfill", ENTRY_FILE],
-    mode: MODE,
-    module: {
-        rules: [{
-                test: /\.(js)$/,
-                use: [{
-                    loader: "babel-loader",
-                }, ],
-            },
-            {
-                test: /\.(scss)$/,
-                use: [{
-                        loader: MiniCssExtractPlugin.loader,
-                    },
-                    {
-                        loader: "css-loader",
-                    },
-                    {
-                        loader: "postcss-loader",
-                        options: {
-                            postcssOptions: {
-                                plugins: [
-                                    [
-                                        autoprefixer,
-                                        {
-                                            //options
-                                            browsers: "cover 99.5%",
-                                        },
-                                    ],
-                                ],
-                            },
-                        },
-                    },
-                    {
-                        loader: "sass-loader",
-                    },
-                ],
-            },
+  devtool: "cheap-module-source-map", // 에러나서 추가 됨
+  entry: ["@babel/polyfill", ENTRY_FILE],
+  mode: MODE,
+  module: {
+    rules: [
+      {
+        test: /\.(js)$/,
+        use: [
+          {
+            loader: "babel-loader",
+          },
         ],
-    },
-    output: {
-        path: OUTPUT_DIR,
-        filename: "[name].js",
-    },
-    plugins: [new MiniCssExtractPlugin({ filename: "styles.css" })],
+      },
+      {
+        test: /\.(scss)$/,
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader,
+          },
+          {
+            loader: "css-loader",
+          },
+          {
+            loader: "postcss-loader",
+            options: {
+              postcssOptions: {
+                plugins: [
+                  [
+                    autoprefixer,
+                    {
+                      //options
+                      browsers: "cover 99.5%",
+                    },
+                  ],
+                ],
+              },
+            },
+          },
+          {
+            loader: "sass-loader",
+          },
+        ],
+      },
+    ],
+  },
+  output: {
+    path: OUTPUT_DIR,
+    filename: "[name].js",
+  },
+  plugins: [new MiniCssExtractPlugin({ filename: "styles.css" })],
 };
 
 module.exports = config;
