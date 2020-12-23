4578b6dd3b95b7b7b2a8e85a4870bcfa685dc83b
KIMJINHWAN <beliemun@paran.com>
2020-12-23 22:17:40 +0900

    Update

--- src/init.js
+++ src/init.js
@@ -7,9 +7,9 @@ import "./models/User";
 
 dotenv.config();
 
-const PORT = process.env.MONGO_URL_PROD;
+const PORT = process.env.PORT || 4000;
 
 const handleListening = () =>
-    console.log(`✅ Listening on: ${PORT}`);
+    console.log(`✅ Listening on: http://localhost:${PORT}`);
 
 app.listen(PORT, handleListening);
