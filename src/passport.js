d2bbd7416e92857fd4949be2bde6e81879a73b85
KIMJINHWAN <beliemun@paran.com>
2020-12-23 21:02:28 +0900

    Set Update

--- src/passport.js
+++ src/passport.js
@@ -19,11 +19,8 @@ passport.use(
     new GithubStrategy({
             clientID: process.env.GITHUB_CLIENT_ID,
             clientSecret: process.env.GITHUB_CLIENT_SECRET,
-            callbackURL: `http://localhost:4000${routes.githubCallback}`,
-            // callbackURL: `https://stormy-hollows-56478.herokuapp.com${routes.githubCallback}`
-            // callbackURL: process.env.PRODUCTION ?
-            //     `https://stormy-hollows-56478.herokuapp.com${routes.githubCallback}` :
-            //     `http://localhost:4000${routes.githubCallback}`
+            callbackURL: process.env.PRODUCTION ?
+                `https://stormy-hollows-56478.herokuapp.com${routes.githubCallback}` : `http://localhost:4000${routes.githubCallback}`
         },
         githubLoginCallback
     )
@@ -32,11 +29,8 @@ passport.use(
     new KakaoStrategy({
             clientID: process.env.KAKAO_CLIENT_ID,
             clientSecret: process.env.KAKAO_CLIENT_SECRET, // clientSecret을 사용하지 않는다면 넘기지 말거나 빈 스트링을 넘길 것
-            callbackURL: `http://localhost:4000${routes.kakaoCallback}`
-                // callbackURL: `https://stormy-hollows-56478.herokuapp.com${routes.kakaoCallback}`
-                // callbackURL: process.env.PRODUCTION ?
-                //     `https://stormy-hollows-56478.herokuapp.com${routes.kakaoCallback}` :
-                //     `http://localhost:4000${routes.kakaoCallback}`
+            callbackURL: process.env.PRODUCTION ?
+                `https://stormy-hollows-56478.herokuapp.com${routes.kakaoCallback}` : `http://localhost:4000${routes.kakaoCallback}`
         },
         kakaoLoginCallback
     )
